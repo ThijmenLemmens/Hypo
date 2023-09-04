568073f0f6191c6424d95f74a1048993d14fdbe5
ThijmenJava <1194425@student.roc-nijmegen.nl>
2023-09-04 14:43:30 +0200

    add combobox

--- src/main/java/com/thijmen/hypo/MainController.java
+++ src/main/java/com/thijmen/hypo/MainController.java
@@ -3,12 +3,20 @@ package com.thijmen.hypo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    public void initialize() {
+
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+
 }
